467b5ba329f7994208c7d414f95c516fab9b0a17
Ольга Шутылева <99029912+shytuleva24@users.noreply.github.com>
2022-06-15 13:11:08 +0300

    asc

--- index.php
+++ index.php
@@ -4,81 +4,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Сomplicated psychic</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Poiret+One&display=swap" rel="stylesheet">
 </head>
 <body>
+    <video autoplay muted loop id="myVideo">
+        <source src="https://vod-progressive.akamaized.net/exp=1655233849~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1767%2F11%2F283838731%2F1067680830.mp4~hmac=348fdb7ed1d29eafbf539644388b24bb5be615d8e9a8330c6b1776aedeb62b10/vimeo-prod-skyfire-std-us/01/1767/11/283838731/1067680830.mp4?filename=Cosmos+-+17692.mp4" type="video/mp4">
+    </video>
     <main>
-        <form action="
-            <?php 
-                session_start();
-                $lose = (isset($_POST['lose'])) ? $_POST['lose'] : 0;
-                $win = (isset($_POST['win'])) ? $_POST['win'] : 0;
-                if ($lose == 1 || $win == 1) {
-                    echo 'two.php';
-                } else {
-                    echo 'index.php';
-                } 
-            ?>" 
-            method="POST">
-            <h1>Гра екстрасенс (ускладненна версія)!</h1>
-            <h2>Спробуй свої можливості</h2>
-            <p class="text">Вгадай число яке загадав комп'ютер! Для цього тобі потрібно лише обрати число з випадаючого списку.</p>
+        <section>
+            <form action="index.php" method="POST">
+                <h1>Гра екстрасенс</h1>
+                <p class="title">Спробуй свої можливості</p>
+                <p class="text">Вгадай діапазон, в якому знаходиться загадане комп'ютером число. Для цього лише потрібно обрати його з випадаючого списку нижче. <br>У тебе все вийде!</p>
+                <?php
+                    session_start();
+                    if (isset($_POST['new'])) {
+                        session_unset();
+                        unset($_SESSION["complicatedPsychic"]);
+                        unset($complicatedPsychic->password);
+                        // session_destroy();
+                    }
 
-            <?php 
-                if (isset($_POST['new'])) {
-                    header("refresh: 0;");
-                    session_destroy();
-                }
+                    class PsychicData {
+                        public $password = 0;
+                        public $win = 0;
+                        public $totalLoss = 0;
+                    }
 
-                $password = (isset($_POST['password'])) ? $_POST['password'] : rand(1, 100);
-                $disabledNumbers = (isset($_SESSION["disabledNumbers"])) ? json_decode($_SESSION["disabledNumbers"]) : [];
-                $password = 74;
-                echo "<p> lose $lose</p>";
-                echo "<h2>$password</h2>";
-                if ($lose < 2) {
+                    $complicatedPsychic = (isset($_SESSION["complicatedPsychic"])) ? json_decode($_SESSION["complicatedPsychic"]) : new PsychicData();
+                    $disabledNumbers = (isset($_SESSION["disabledNumbers"])) ? json_decode($_SESSION["disabledNumbers"]) : [];
+            
+                    if ($complicatedPsychic->password != 0) {
+                        $password = $complicatedPsychic->password;
+                    } else {
+                        $password = rand(1, 100);
+                        $complicatedPsychic->password = $password;
+                    }
+                    echo $complicatedPsychic->password;
+
+
+                    // echo $complicatedPsychic->password;
                     if (isset($_POST['number'])) {
                         $number = $_POST['number'];
-                        explode(" - ", $number);
-                        $start = explode(" - ", $number)[0];
-                        $end = explode(" - ", $number)[1];
-                        if ($password >= $start && $password <= $end) {
-                            echo "<p>WIN</p>";
-                            $win = 1;
-                            echo "win $win <br>";
-                        } else {
-                            $lose++;
-                            array_push($disabledNumbers, $number);
-                            echo "<p>LOSE</p>";
-                        }
-                    }
-                    var_dump ($disabledNumbers);
-
-                    echo ("<p>");
-                    echo ("<select name='number'>");
-                        for($i = 0; $i <= 99; $i+=10) {
-                            $start = $i + 1;
-                            $end = $i + 10;
-                            $value = $start ." - ". $end;
-                            if (in_array($value, $disabledNumbers)) {
-                                echo ("<option value='$value' disabled>$value</option>");
-                            } 
-                            else {
-                                echo ("<option value='$value'>$value</option>");
+                            explode(" - ", $number);
+                            $start = explode(" - ", $number)[0];
+                            $end = explode(" - ", $number)[1];
+                            if ($password >= $start && $password <= $end) {
+                                $complicatedPsychic->win++;
+                                header('Location: two.php');
+                            } else {
+                                $complicatedPsychic->totalLoss++;
+                                array_push($disabledNumbers, $number);
+                                echo "<p>Ви не вгадали, спробуйте ще!</p>";
+                                if ($complicatedPsychic->totalLoss == 3) {
+                                    header('Location: two.php');
+                                }
                             }
                         }
-                    echo ("</select>");
-                    echo ("</p>"); 
-                    
-                }
-                $_SESSION["disabledNumbers"] = json_encode($disabledNumbers);
-            ?>
-            <input class="btn b" type="submit" name="sub" value="Спробувати"></p>
-            <input type="hidden" name="password" value="<?php echo($password) ?>"><br>
-            <input type="hidden" name="lose" value="<?php echo($lose) ?>"><br>
-            <input type="hidden" name="win" value="<?php echo($win) ?>"><br>
+                        echo ("<p>");
+                        echo ("<select class='select' name='number'>");
+                        echo ("<option value='0'>Обери діапазон</option>");
+                            for($i = 0; $i <= 99; $i+=10) {
+                                $start = $i + 1;
+                                $end = $i + 10;
+                                $value = $start ." - ". $end;
+                                if (in_array($value, $disabledNumbers)) {
+                                    echo ("<option value='$value' disabled>$value</option>");
+                                }
+                                else {
+                                    echo ("<option value='$value'>$value</option>");
+                                }
+                            }
+                        echo ("</select>");
 
-            <button class="btn t" type="submit" name="new">Нова гра!</button>
-        </form>
+                    $_SESSION["complicatedPsychic"] = json_encode($complicatedPsychic);
+                    $_SESSION["disabledNumbers"] = json_encode($disabledNumbers);
+                ?>
+                <input class="btn b" type="submit" name="sub" value="Спробувати"></p>
+            </form>
+        </section>
     </main>
 </body>
 </html>
--- two.php
+++ two.php
@@ -4,71 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Сomplicated psychic</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Poiret+One&display=swap" rel="stylesheet">
 </head>
 <body>
-<main>
-        <form action="
-            <?php 
-                session_start();
-                $lose = (isset($_POST['lose'])) ? $_POST['lose'] : 0;
-                $win = (isset($_POST['win'])) ? $_POST['win'] : 0;
-                if ($lose < 4) {
-                    echo 'two.php';
-                } else if ($lose == 4) {
-                    echo 'three.php';
-                } 
-            ?>" 
-            method="POST">
-            <h1>Гра екстрасенс (ускладненна версія)!</h1>
-            <h2>2</h2>
-            <!-- <p class="text">Вгадай число яке загадав комп'ютер! Для цього тобі потрібно лише обрати число з випадаючого списку.</p> --> 
+    <video autoplay muted loop id="myVideo">
+      <source src="https://vod-progressive.akamaized.net/exp=1655233849~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1767%2F11%2F283838731%2F1067680830.mp4~hmac=348fdb7ed1d29eafbf539644388b24bb5be615d8e9a8330c6b1776aedeb62b10/vimeo-prod-skyfire-std-us/01/1767/11/283838731/1067680830.mp4?filename=Cosmos+-+17692.mp4" type="video/mp4">
+    </video>
+    <main>
+        <section>
+        <form action="three.php" method="POST">
+            <h1>Гра екстрасенс</h1>
+            <p class="title">Спробуй свої можливості</p>
+                <?php 
+                    session_start();
+                    $complicatedPsychic = json_decode($_SESSION['complicatedPsychic']);
+                    $disabledNumbers = (isset($_SESSION['disabledNumbers'])) ? json_decode($_SESSION['disabledNumbers']) : [];
+                    $password = $complicatedPsychic->password;
+                    $loss = 0;
 
-            <?php 
-                $password = (isset($_POST['password'])) ? $_POST['password'] : rand(1, 100);
-                $disabledNumbers = (isset($_SESSION["disabledNumbers"])) ? json_decode($_SESSION["disabledNumbers"]) : [];
-                echo "<h2>$password</h2>";
-                echo "<p> lose $lose</p>";
-                if ($lose < 5) {
-                    $numberStart = $password[0] . 0;
-                    if ($password == 100) {
-                        $numberStart = 90;
+                    $numberStart = floor(($complicatedPsychic->password-1)/10)*10+1;
+                    $numberEnd = $numberStart + 9;
+
+                
+                    if ($complicatedPsychic->totalLoss == 0) {
+                        echo "<p class='text'>В тебе дуже гарно виходить, ти вгадав, так тримати!</p>";
+                    } else if (count($disabledNumbers) != 0 && $complicatedPsychic->win == 0) {
+                        echo "<p class='text'>Шкода, але ти не вгадав!</p>";
                     }
-                    $numberEnd = $numberStart + 10;
-                    echo $numberStart;
-                    echo $numberEnd;
 
+                    echo "<p class='text'>Тепер спробуй вгадати саме число. В тебе є лише одна спроба тому добре подумай та обери його зі списку нижче.<br>У тебе все вийде!</p>";
+                    echo $complicatedPsychic->password;
+                    
                     if (isset($_POST['number'])) {
                         $number = $_POST['number'];
                         if ($password == $number) {
-                            echo "<p>WIN</p>";
+                            $complicatedPsychic->win++;
+                            header('Location: result.php');
                         } else {
-                            $lose++;
+                            $complicatedPsychic->totalLoss++;
                             array_push($disabledNumbers, $number);
-                            echo "<p>LOSE</p>";
                         }
-                        echo ("<p>");
-                        echo ("<select name='number'>");
-                            for($i = $numberStart; $i <= $numberEnd; $i++) {
-                                if (in_array($i, $disabledNumbers)) {
-                                    echo ("<option value='$i' disabled>$i</option>");
-                                } 
-                                else {
-                                    echo ("<option value='$i'>$i</option>");
-                                }
-                            }
-                        echo ("</select>");
-                        echo ("</p>"); 
                     }
-                    var_dump ($disabledNumbers);
-                }
-                $_SESSION["disabledNumbers"] = json_encode($disabledNumbers);
-            ?>
-            <input class="btn b" type="submit" name="sub" value="Спробувати"></p>
-            <input type="hidden" name="password" value="<?php echo($password) ?>"><br>
-            <input type="hidden" name="lose" value="<?php echo($lose) ?>"><br>
-            <button class="btn t" type="submit" name="new"><a href="index.php">Нова гра!</a></button>
-        </form>
+                    
+                    var_dump($disabledNumbers);
+                    echo ("<p>");
+                    echo ("<select class='select' name='number'>");
+                    echo ("<option value='0'>Обери число</option>");
+                        for ($i = $numberStart; $i <= $numberEnd; $i++) {
+                            if (in_array($i, $disabledNumbers)) {
+                                echo ("<option value='$i' disabled>$i</option>");
+                            } else {
+                                echo ("<option value='$i'>$i</option>");
+                            }
+                        }
+                    echo ("</select>");
+                    echo "<input class='btn b' type='submit' name='sub' value='Спробувати'></p>";
+                    $_SESSION["complicatedPsychic"] = json_encode($complicatedPsychic);
+                    $_SESSION["disabledNumbers"] = json_encode($disabledNumbers);
+                ?>                <!-- <button class="btn t" type="submit" name="new"><a href="index.php">Нова гра!</a></button> -->
+            </form>
+        </section>
     </main>
 </body>
 </html>
+
